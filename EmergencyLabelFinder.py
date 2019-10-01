320834951e02ccc427b75f19ea11d1bd7bbfd4a3
Taylor Sahli <taylorsahli837@gmail.com>
2019-10-01 07:59:13 -0600

    Now allows user to enter in values to seperate rather than being hard coded in

--- EmergencyLabelFinder.py
+++ EmergencyLabelFinder.py
@@ -2,7 +2,16 @@
 # EmergencyLabelFinder.py - separates cells searched for from those that don't meet criteria
 
 import openpyxl, pprint, time
-excelSheetName = raw_input('Enter Name of Excel Sheet: ')
+excelSheetName = input('Enter Name of Excel Sheet: ')
+startingLetters = []
+startingLetter = ''
+print("Enter starting letters to filter onto a seperate tab. Enter 'done' when finished.")
+
+while startingLetter != 'done':
+    startingLetter = input("Enter a starting letter: ")
+    if startingLetter != 'done':
+        startingLetter = startingLetter.upper()
+        startingLetters.append(startingLetter)
 
 wb = openpyxl.load_workbook(excelSheetName + '.xlsx')
 sheet = wb.get_sheet_by_name('Sheet1')
@@ -16,7 +25,7 @@ next_row = 1
 for row in range(1, sheet.max_row + 1):
     label = sheet['A' + str(row)].value
     labelString = str(label) # This variable might not be needed
-    if labelString.startswith("E") or labelString.startswith("L") or labelString.startswith("C"):
+    if labelString.startswith(tuple(startingLetters)):
         createdEMsheet.cell(column = 1, row = next_EM_row, value = label)
         next_EM_row += 1
     elif label: # Using implicit booleanness of label object. If false skip
@@ -25,4 +34,4 @@ for row in range(1, sheet.max_row + 1):
 
 wb.save(excelSheetName + '.xlsx')
 print('------------------------Done------------------------')
-time.sleep(5)
+time.sleep(3)
